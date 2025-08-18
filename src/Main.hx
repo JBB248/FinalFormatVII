8c6c050ebdac3ff214abd06cbad1bf3e0d810631
JBB248 <joebray248@outlook.com>
2025-08-18 14:06:23 -0500

    Initialize project

---
+++ src/Main.hx
@@ -0,0 +1,30 @@
+package;
+
+import flixel.FlxGame;
+import flixel.util.typeLimit.NextState;
+
+import haxe.ui.Toolkit;
+
+class Main extends openfl.display.Sprite
+{
+    var initialState:InitialState = PlayState.new;
+
+	public function new()
+	{
+		super();
+
+        initHaxeUI();
+
+		addChild(new FlxGame(0, 0, PlayState));
+	}
+
+    function initHaxeUI():Void
+    {
+        Toolkit.init();
+        Toolkit.theme = "dark-compact";
+        Toolkit.autoScale = false;
+
+        haxe.ui.focus.FocusManager.instance.autoFocus = false;
+        haxe.ui.tooltips.ToolTipManager.defaultDelay = 800;
+    }
+}
