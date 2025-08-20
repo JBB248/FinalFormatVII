ec9facafe4fb8e0cb282afb4235e50ebbbe63f6c
JBB248 <joebray248@outlook.com>
2025-08-20 17:03:52 -0500

    Add invalid image erroring

--- src/PlayState.hx
+++ src/PlayState.hx
@@ -1,18 +1,20 @@
 package;
 
-import openfl.display.BitmapData;
 import flixel.FlxG;
 import flixel.FlxSprite;
 
-import haxe.ui.events.MouseEvent;
+import haxe.ui.backend.flixel.UIState;
 import haxe.ui.containers.dialogs.Dialog;
 import haxe.ui.containers.dialogs.Dialogs;
 import haxe.ui.containers.dialogs.OpenFileDialog;
+import haxe.ui.events.MouseEvent;
+
+import openfl.display.BitmapData;
 
 using StringTools;
 
 @:build(haxe.ui.ComponentBuilder.build("src/components/main.xml"))
-class PlayState extends haxe.ui.backend.flixel.UIState
+class PlayState extends UIState
 {
     var dialog = new OpenFileDialog();
 
@@ -47,17 +49,35 @@ class PlayState extends haxe.ui.backend.flixel.UIState
     {
         if(event.button != DialogButton.OK) return;
 
+        var dpi = -1;
         var bytes = dialog.selectedFiles[0].bytes;
+        var path = dialog.selectedFiles[0].fullPath.toLowerCase();
+        if(path.endsWith("png"))
+        {
+            dpi = DPIInterpreter.fromPNG(bytes);
+        }
+        else if(path.endsWith("gif"))
+        {
+            trace("gif file extension is not supported. Please convert the image into png, or jpg");
+            return;
+        }
+        else if(path.endsWith("bmp"))
+        {
+            trace("bmp file extension is not supported. Please convert the image into png, or jpg");
+            return;
+        }
+        else
+        {
+            trace("Invalid file submitted");
+            return;
+        }
+
+        dpiLabel.text = "DPI: " + dpi;
+
         var bitmap = BitmapData.fromBytes(bytes);
         coverSprite.loadGraphic(bitmap);
         coverSprite.revive();
         centerCoverSprite();
-
-        var dpi = -1;
-        if(dialog.selectedFiles[0].fullPath.toLowerCase().endsWith(".png"))
-            dpi = DPIInterpreter.fromPNG(bytes);
-
-        dpiLabel.text = "DPI: " + dpi;
     }
 
     function centerCoverSprite():Void
